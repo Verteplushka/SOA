b503577ab8092ceadd92d980fe466e65d9dff889
Karabas890 <ivan.mitichev@mail.ru>
2025-12-06 07:43:54 +0300

    откат

--- NewService2/api-gateway/src/main/java/edu/itmo/soa/newservice2/apigateway/ApiGatewayApplication.java
+++ NewService2/api-gateway/src/main/java/edu/itmo/soa/newservice2/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package edu.itmo.soa.newservice2.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
