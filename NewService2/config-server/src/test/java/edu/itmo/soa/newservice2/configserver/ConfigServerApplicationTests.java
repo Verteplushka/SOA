83f59462508837ea8a471434433db433e9039598
Karabas890 <ivan.mitichev@mail.ru>
2025-12-06 04:51:43 +0300

    v1

---
+++ NewService2/config-server/src/test/java/edu/itmo/soa/newservice2/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itmo.soa.newservice2.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
