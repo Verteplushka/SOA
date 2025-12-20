187d37445c5b9f4fe587bdf74a516f48842e3344
Karabas890 <ivan.mitichev@mail.ru>
2025-12-20 05:53:30 +0300

    cors

--- NewService2/api-gateway/src/main/java/edu/itmo/soa/newservice2/apigateway/config/Config.java
+++ NewService2/api-gateway/src/main/java/edu/itmo/soa/newservice2/apigateway/config/Config.java
@@ -2,12 +2,8 @@ package edu.itmo.soa.newservice2.apigateway.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @Configuration
 @ComponentScan("edu.itmo.soa.newservice2.apigateway")
-@Import({WebConfig.class})
 public class Config {
 }
--- NewService2/api-gateway/src/main/java/edu/itmo/soa/newservice2/apigateway/config/WebConfig.java
+++ NewService2/api-gateway/src/main/java/edu/itmo/soa/newservice2/apigateway/config/WebConfig.java
@@ -1,22 +1,17 @@
 package edu.itmo.soa.newservice2.apigateway.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
