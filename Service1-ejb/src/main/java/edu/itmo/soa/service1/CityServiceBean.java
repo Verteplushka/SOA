1f41d17c920743740b1034db03dee2a0ea1c2f23
Verteplushka <kolbacin_vlad@mail.ru>
2025-12-20 03:16:41 +0300

    Added some consul

--- Service1-ejb/src/main/java/edu/itmo/soa/service1/CityServiceBean.java
+++ Service1-ejb/src/main/java/edu/itmo/soa/service1/CityServiceBean.java
@@ -15,7 +15,9 @@ import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.annotation.PostConstruct;
+import org.jboss.logging.Logger;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -31,32 +33,6 @@ import java.util.List;
 public class CityServiceBean implements CityServiceRemote {
     @PersistenceContext(unitName = "CityPU")
     private EntityManager em;
-
-    @PostConstruct
-    public void registerInConsul() {
-        try {
-            String serviceJson = "{"
-                    + "\"ID\": \"service1\","
-                    + "\"Name\": \"service1\","
-                    + "\"Address\": \"localhost\","
-                    + "\"Port\": 8080,"
-                    + "\"Tags\": [\"ejb\",\"remote\"]"
-                    + "}";
-
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:8500/v1/agent/service/register"))
-                    .header("Content-Type", "application/json")
-                    .PUT(HttpRequest.BodyPublishers.ofString(serviceJson))
-                    .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Consul registration response: " + response.body());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     @Override
     public City findById(int id) {
@@ -290,13 +266,27 @@ public class CityServiceBean implements CityServiceRemote {
         Comparator<City> comparator = Comparator.comparing(city -> {
             Object value;
             switch (field) {
-                case "id": value = city.getId(); break;
-                case "name": value = city.getName(); break;
-                case "area": value = city.getArea(); break;
-                case "population": value = city.getPopulation(); break;
-                case "metersAboveSeaLevel": value = city.getMetersAboveSeaLevel(); break;
-                case "populationDensity": value = city.getPopulationDensity(); break;
-                case "government": value = city.getGovernment(); break;
+                case "id":
+                    value = city.getId();
+                    break;
+                case "name":
+                    value = city.getName();
+                    break;
+                case "area":
+                    value = city.getArea();
+                    break;
+                case "population":
+                    value = city.getPopulation();
+                    break;
+                case "metersAboveSeaLevel":
+                    value = city.getMetersAboveSeaLevel();
+                    break;
+                case "populationDensity":
+                    value = city.getPopulationDensity();
+                    break;
+                case "government":
+                    value = city.getGovernment();
+                    break;
                 case "governor.age":
                     value = city.getGovernor() != null ? city.getGovernor().getAge() : null;
                     break;
@@ -306,9 +296,15 @@ public class CityServiceBean implements CityServiceRemote {
                 case "coordinates.y":
                     value = city.getCoordinates() != null ? city.getCoordinates().getY() : null;
                     break;
-                case "establishmentDate": value = city.getEstablishmentDate(); break;
-                case "creationDate": value = city.getCreationDate(); break;
-                default: value = null; break;
+                case "establishmentDate":
+                    value = city.getEstablishmentDate();
+                    break;
+                case "creationDate":
+                    value = city.getCreationDate();
+                    break;
+                default:
+                    value = null;
+                    break;
             }
             return (Comparable) value;
         }, Comparator.nullsLast(Comparator.naturalOrder()));
@@ -319,7 +315,6 @@ public class CityServiceBean implements CityServiceRemote {
 
         cities.sort(comparator);
     }
-
 
 
     private <T extends Comparable<T>> boolean inRange(CitySearchRequest.Range<T> range, T value) {
