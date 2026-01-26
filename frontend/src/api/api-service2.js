e7ddf948eb9463a4194f44252ef2b0e2e01d021b
Karabas890 <ivan.mitichev@mail.ru>
2026-01-26 06:54:13 +0300

    откат

--- frontend/src/api/api-service2.js
+++ frontend/src/api/api-service2.js
@@ -3,7 +3,7 @@ import { XMLParser, XMLBuilder } from "fast-xml-parser";
 
 const BASE_URL =
   import.meta.env.VITE_GENOCIDE_API_BASE ||
-  "https://158.160.138.235:8766/REST_layer/genocide";
+  "https://158.160.138.235:8090/genocide";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
